25b15808b1f23b9b6fea67af0cc0f2b8d7bdaefb
Fabio-Frare <nabata99@hotmail.com>
2019-09-17 20:58:25 -0300

    Adicionado uma variavel teste.

--- app/src/main/java/br/udesc/menudigital/InicialActivity.java
+++ app/src/main/java/br/udesc/menudigital/InicialActivity.java
@@ -10,5 +10,7 @@ public class InicialActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicial);
+
+        String teste;
     }
 }
